634a881f3c90b319586e030b5fec9fd652e3431f
khpeet <kpeet@newrelic.com>
2024-07-18 09:40:18 -0400

    update password scripted_browser

--- scripts/scripted_browser.js
+++ scripts/scripted_browser.js
@@ -11,7 +11,7 @@ var elementWaitTimer = 10000;
 
 const url_to_monitor = 'https://www.tutorialrepublic.com/snippets/preview.php?topic=bootstrap&file=simple-login-form';
 const user = 'keagan'; //$secure.user
-const pass = 'test3';
+const pass = 'test4';
 /** CONFIGURATION **/
 
 async function stepOne() { //get homepage + validate element on page
